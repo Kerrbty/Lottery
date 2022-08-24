2da5cccb5cc457a42a251f2b5751d46cd5490601
luguojie <jha334201553@163.com>
2022-08-24 17:58:57 +0800

    数据库用压缩的excel就行

---
+++ src/DataBase.cpp
@@ -0,0 +1,153 @@
+#include "DataBase.h"
+#include "zlib/zlib.h"
+
+#define DATA_BASE_FILE TEXT("Data.bin")
+
+BOOL ReadFromFile(LPTSTR szFileName, unsigned char** buf, unsigned int* len)
+{
+    BOOL bRet = FALSE;
+    HANDLE hFile = CreateFile( 
+        szFileName,
+        GENERIC_READ, 
+        FILE_SHARE_READ, 
+        NULL, 
+        OPEN_EXISTING, 
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        return bRet;
+    }
+
+    // 读取整个文件 
+    DWORD dwSize = GetFileSize(hFile, NULL);
+    unsigned char* pibuf = new unsigned char[dwSize];
+    if (pibuf)
+    {
+        DWORD dwBytes = 0;
+        SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
+        ReadFile(hFile, pibuf, dwSize, &dwBytes, NULL);
+        
+        *buf = pibuf;
+        *len = dwBytes;
+        bRet = TRUE;
+    }
+    CloseHandle(hFile);
+
+    return bRet;
+}
+
+BOOL WriteToFile(LPTSTR szFileName, unsigned char* buf, unsigned int len)
+{
+    HANDLE hFile = CreateFile( 
+        szFileName,
+        GENERIC_WRITE, 
+        FILE_SHARE_READ, 
+        NULL, 
+        CREATE_ALWAYS, 
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        return FALSE;
+    }
+
+    // 读取整个文件 
+    DWORD dwBytes = 0;
+    SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
+    WriteFile(hFile, buf, len, &dwBytes, NULL);
+    CloseHandle(hFile);
+
+    return TRUE;
+}
+
+VOID FreeData(void* pb)
+{
+    delete []pb;
+}
+
+BOOL SaveFile2Database(LPTSTR lpFileName)
+{
+    BOOL bRet = FALSE;
+    unsigned char* pibuf = NULL;
+    unsigned int len = 0;
+    if (ReadFromFile(lpFileName, &pibuf, &len))
+    {
+        DWORD dwCompressLen = len;
+        unsigned char* pobuf = new unsigned char[dwCompressLen];
+        if (pobuf)
+        {
+            // 压缩 
+            if( compress(pobuf, &dwCompressLen, pibuf, len) == Z_OK)
+            {
+                // 保存文件 
+                bRet = WriteToFile(DATA_BASE_FILE, pobuf, dwCompressLen);
+            }
+            delete []pobuf;
+        }
+        FreeData(pibuf);
+    }  
+    return bRet;
+}
+
+BOOL ReadDatabase(unsigned char** pb, unsigned int* size)
+{
+    BOOL bRet = FALSE;
+    unsigned char* pibuf = NULL;
+    unsigned int len = 0;
+    if (ReadFromFile(DATA_BASE_FILE, &pibuf, &len))
+    {
+        int n = 1;
+        DWORD dwLen = len*2*n;
+        unsigned char* pobuf = new unsigned char[dwLen];
+        do 
+        {
+            if (pobuf == NULL)
+            {
+                break;
+            }
+
+            // 压缩 
+            int res = uncompress(pobuf, &dwLen, pibuf, len);
+            if(res == Z_OK)
+            {
+                *pb = pobuf;
+                *size = dwLen;
+                bRet = TRUE;
+                break;
+            }
+            else if (res == Z_BUF_ERROR)
+            {
+                delete []pobuf;
+
+                n++;
+                dwLen = len*2*n;
+                pobuf = new unsigned char[dwLen];
+            }
+            else
+            {
+                break;
+            }
+        } while (TRUE);
+        FreeData(pibuf);
+    }  
+    return bRet;
+}
+
+#ifdef _TEST_DATABASE_HEADER_H_
+
+int main()
+{
+    // 保存数据库 
+    SaveFile2Database(TEXT("名单.xls"));
+
+    // 读取数据库 
+    unsigned char* pibuf = NULL;
+    unsigned int len = 0;
+    ReadDatabase(&pibuf, &len);
+    FreeData(pibuf);
+
+    return 0;
+}
+
+#endif // _TEST_DATABASE_HEADER_H_
---
+++ src/DataBase.h
@@ -0,0 +1,27 @@
+#ifndef _DATABASE_HEADER_FILE_H_
+#define _DATABASE_HEADER_FILE_H_
+#include <Windows.h>
+#include <tchar.h>
+
+#ifdef _cplusplus
+extern "C" {
+#endif // _cplusplus 
+
+// 从文件中读数据，返回buffer需要 FreeData 释放 
+BOOL ReadFromFile(LPTSTR szFileName, unsigned char** buf, unsigned int* len);
+// 将buffer写入指定文件 
+BOOL WriteToFile(LPTSTR szFileName, unsigned char* buf, unsigned int len);
+// 将文件加密存为数据库 
+BOOL SaveFile2Database(LPTSTR lpFileName);
+// 从数据库中读取buffer 
+BOOL ReadDatabase(unsigned char** pb, unsigned int* size);
+// 释放内存 
+VOID FreeData(void* pb);
+
+
+
+#ifdef _cplusplus
+};
+#endif // _cplusplus
+
+#endif  // _DATABASE_HEADER_FILE_H_ 
